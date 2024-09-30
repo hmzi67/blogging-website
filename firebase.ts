3e3eee7231099aabb76736e9b84a1e9afb07cf35
hmzi67 <hamzawaheed057@gmail.com>
2024-10-01 02:28:29 +0500

    securing credentials

--- firebase.ts
+++ firebase.ts
@@ -3,19 +3,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"; // Import Storage
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCBDTGhRZ4Soz36M8BlmvtlndoOgtfax80",
-  authDomain: "blogging-763da.firebaseapp.com",
-  projectId: "blogging-763da",
-  storageBucket: "blogging-763da.appspot.com",
-  messagingSenderId: "67688640113",
-  appId: "1:67688640113:web:fa0daa5e800d6f879775d0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app); // Initialize Storage
-
 
 export { db, storage };
